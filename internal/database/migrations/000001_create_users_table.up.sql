fabee63c7594e3b6a188a5474a8a4eb043af0743
Niiaks <napappoe007@st.ug.edu.gh>
2026-01-13 20:28:36 +0000

    Format SQL migration files for consistent indentation

--- internal/database/migrations/000001_create_users_table.up.sql
+++ internal/database/migrations/000001_create_users_table.up.sql
@@ -1,10 +1,10 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
-                       id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                       name VARCHAR(255) NOT NULL,
-                       email VARCHAR(255) UNIQUE NOT NULL,
-                       password VARCHAR(255) NOT NULL,
-                       created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-                       updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
